2e735b8571eddbf8bb74a077b0ae5073f414d654
jmeister50 <j.c.meister50@gmail.com>
2020-02-23 21:11:09 -0600

    Created function for XPath Clicking and started date functionality

--- marriot.py
+++ marriot.py
@@ -10,31 +10,42 @@ import config as cfg
 url = cfg.url
 browser = webdriver.Chrome(cfg.driver)
 
-def search_city(city):
+def wait_until_clickable_xpath(xpath):
+
+	element = WebDriverWait(browser, 20).until(
+		EC.element_to_be_clickable((By.XPATH, str(xpath)))
+		)
+	element.click()
+
+def search_city(city, state, start_date, end_date):
 	
+	# Go to starting url
 	browser.get(url)
 
-	element = WebDriverWait(browser, 20).until(
-		EC.element_to_be_clickable((By.XPATH, '//*[@id="advanced-search-form"]/div/div[1]'))
-		)
+	# Clicking Destination Form Box. 
+ 	wait_until_clickable_xpath('//*[@id="advanced-search-form"]/div/div[1]')
+
+ 	# Sending City and State to destination box. 
+	browser.find_element_by_name('destinationAddress.destination').send_keys(city + ', ' + state)
 	element.click()
 
-	browser.find_element_by_name('destinationAddress.destination').send_keys(city)
+	# TODO: Finish date functionality
+	element = browser.find_elements_by_class_name('ccheckin')
 
-	element = WebDriverWait(browser, 20).until(
-		EC.element_to_be_clickable((By.XPATH, '//*[@id="advanced-search-form"]/div/div[9]/button'))
-		)
-	element.click()
+	# TODO: Add filtering logic here
+	# Clicking the Search Button.
+	wait_until_clickable_xpath('//*[@id="advanced-search-form"]/div/div[9]/button')
 
-	#TODO: Add filtering logic here
-
+	# Create list of Hotel names and Rates. 
 	hotel_names = browser.find_elements_by_class_name('l-property-name')
 	hotel_prices = browser.find_elements_by_class_name('m-display-block')
-	
+
+	# Append name of Hotel to list.
 	names = []
 	for name in hotel_names:
 		names.append(name.text)
 
+	# Append Rate to list. 
 	prices = []
 	for price in hotel_prices:
 		if len(price.text) > 0:
@@ -42,7 +53,8 @@ def search_city(city):
 		else:
 			pass
 
+	# Pairs names and rates in a Dictionary.
 	hotel_info = dict(zip(names,prices))
 	print(hotel_info)
 
-search_city('Chicago, IL')
+search_city('Chicago', 'IL', 1, 1)
